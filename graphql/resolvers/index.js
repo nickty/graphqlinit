3f10329aedd57c4ae02fdaeb65d37feeb42c32d7
Mizanur Rahman <nickty.86@gmail.com>
2021-03-17 08:26:11 +0600

    register new user

---
+++ graphql/resolvers/index.js
@@ -0,0 +1,11 @@
+const postsResolves = require('./posts')
+const userResolvers = require('./users')
+
+module.exports = {
+    Query: {
+        ...postsResolves.Query
+    },
+    Mutation:{
+        ...userResolvers.Mutation
+    }
+}
