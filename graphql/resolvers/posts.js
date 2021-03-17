682f091ef6884d5e1c5581a85446837b7401b531
Mizanur Rahman <nickty.86@gmail.com>
2021-03-17 10:04:19 +0600

    get single post from DB

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -9,6 +9,19 @@ module.exports = {
             } catch (error) {
                 throw new Error(error);
             }
+        },
+        
+        async getPost(_, {postId}){
+            try {
+                const post = await Post.findById(postId)
+                if(post){
+                    return post;
+                } else {
+                    throw new Error('Post not found')
+                }
+            } catch (error) {
+                throw new Error(error)
+            }
         }
     }
 }
